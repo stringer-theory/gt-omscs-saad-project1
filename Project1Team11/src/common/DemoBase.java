a039422e06d1c6cd549193a3c0a0a3ee4d009c52
Travis Loncar <loncar.travis@gmail.com>
2014-09-18 00:21:55 -0400

    Refactor switch into conditional. Extract range confinement.

--- Project1Team11/src/common/DemoBase.java
+++ Project1Team11/src/common/DemoBase.java
@@ -23,59 +23,40 @@ public class DemoBase{
 	}
 
 	protected void processArgs(String[] args){
-		// insure args are passed as pairs (flag and value)
+		// ensure args are passed as pairs (flag and value)
 		if (args.length>0 && args.length % 2 == 0) {
 			// parse each pair and assign value to appropriate variable
-			for (int loop = 0; loop < args.length; loop = loop + 2) {
-				switch (args[loop]) { 
-				// dimension
-				case "-d":	
-					dimension = Integer.parseInt(args[loop + 1]);
-					break;
-				// left edge (range 0-100)	
-				case "-l":
-					left = Integer.parseInt(args[loop + 1]);
-					if (left < 0) {
-						left = 0;
-					} else if (left > 100) {
-						left = 100;
-					}
-					break;
-				// right edge (range 0-100)
-				case "-r":
-					right = Integer.parseInt(args[loop + 1]);
-					if (right < 0) {
-						right = 0;
-					} else if (right > 100) {
-						right = 100;
-					}
-					break;
-				// top edge (range 0-100)
-				case "-t":
-					top = Integer.parseInt(args[loop + 1]);
-					if (top < 0) {
-						top = 0;
-					} else if (top > 100) {
-						top = 100;
-					}
-					break;
-				// bottom edge (range 0-100)
-				case "-b":
-					bottom = Integer.parseInt(args[loop + 1]);
-					if (bottom < 0) {
-						bottom = 0;
-					} else if (bottom > 100) {
-						bottom = 100;
-					}
-					break;
-				// unknown flag	
-				default:
+			for (int i = 0; i < args.length; i = i + 2) {
+				String flag = args[i];
+				
+				if(flag.equals("-d")) {	
+					dimension = Integer.parseInt(args[i + 1]);
+				} else if(flag.equals("-l")) {
+					left = ensureInRange(args[i + 1]);
+				} else if(flag.equals("-r")) {
+					right = ensureInRange(args[i + 1]);
+				} else if(flag.equals("-t")) {
+					top = ensureInRange(args[i + 1]);
+				} else if(flag.equals("-b")) {
+					bottom = ensureInRange(args[i + 1]);
+				} else {
 					usage();
 				}
 			}
 		} else {
 			usage();
 		}
+	}
+	
+	// Confine edge temperature to [0, 100] range
+	protected int ensureInRange(String edgeTemp) {
+		int tmpTemp = Integer.parseInt(edgeTemp);
+		if (tmpTemp < 0) {
+			tmpTemp = 0;
+		} else if (tmpTemp > 100) {
+			tmpTemp = 100;
+		}
+		return tmpTemp;
 	}
 
 	protected void usage(){
